795763a2d195610559f694276b2b88150ca8b9f9
12px <dalton@12px.io>
2018-02-11 14:24:44 -0600

    rename tabs to steps.

--- engine/vue/routing.js
+++ engine/vue/routing.js
@@ -1,20 +1,20 @@
 /* bloks engine: routing */
 
 // Page (Tab) Template
-Vue.component('tab', {
+Vue.component('step', {
   props: ['name'],
   template: `<div class="tab-pane fade" role="tabpanel" :id="name"
     :aria-labelledby="name + 'Link'"><slot></slot></div>`
 });
 
 // Page Links For Sidebar
-Vue.component('tab-link', {
+Vue.component('step-link', {
   props: ['to'],
   template: `<a :href="'#' + to" :id="to + 'Link'" class="nav-link" data-toggle="pill" aria-selected="false" :aria-controls="'#' + to"><slot></slot></a>`
 });
 
 // Page Buttons For Use Anywhere
-Vue.component('tab-button', {
+Vue.component('step-button', {
   props: ['to'],
   methods: { tab: function(tab) { $('#'+tab+'Link').tab('show'); } },
   template: `<a href="#" @click="tab(to)" class="btn mr-3 btn-outline-primary mt-3"><slot></slot></a>`
